26fd2eb81345660abb20a99eee0bd2c16a4a52b5
fauzan1892 <fauzanfalah21@gmail.com>
2019-07-22 12:13:22 +0700

    BS 4 card with image left of header and title
    Bootstrap 4 card/panel with image left of header and title
    https://www.codeply.com/go/l1KAQtjjbA
    https://www.codeply.com/go/l1KAQtjjbA
    Source : https://stackoverflow.com/questions/49141874/bootstrap-4-card-panel-with-image-left-of-header-and-title

---
+++ panel_card_left.php
@@ -0,0 +1,36 @@
+
+
+
+
+   <div class="card flex-row flex-wrap">
+        <div class="card-header border-0">
+            <img src="//placehold.it/200" alt="">
+        </div>
+        <div class="card-block px-2">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Description</p>
+            <a href="#" class="btn btn-primary">BUTTON</a>
+        </div>
+        <div class="w-100"></div>
+        <div class="card-footer w-100 text-muted">
+            FOOTER
+        </div>
+    </div>
+
+  <div class="card">
+        <div class="row no-gutters">
+            <div class="col-auto">
+                <img src="//placehold.it/200" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+                <div class="card-block px-2">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Description</p>
+                    <a href="#" class="btn btn-primary">BUTTON</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer w-100 text-muted">
+            Footer stating cats are CUTE little animals
+        </div>
+  </div>
